8b42f5cafffa263b3a7dfae5881c3a951ed536b0
Vidhur Prabhu <vidhur.prabhu@gmail.com>
2021-02-02 19:02:39 -0800

    Added BStats (?)
    Planned Patch Notes:
    
    - Added Custom Vault Economy
    - Added Feature To Teleport The Player To The Lobby On Join
    - Added Placeholder API Support For All Config Values
    - Added API Support
    - Added Grief Protection
    - Added Grief Protection Regions
    - Added 12 New Commands --> /forcejoin, /forceleave, /togglechat, /sun, /rain, /kickall, /banall, /suicide, /protect, /feed, /heal, and /gm
    - /forcejoin Fakes A Player To Join, And Sends A Message To Everyone In The Server That The Player Has Joined
    - /forceleave Does The Same Thing As /forcejoin, But Makes The Player Leave Instead. If The Targeted Player Is Online, Then It Will Kick Them With A Customizable Error Message.
    - /togglechat Allows The Player To Toggle Receiving Messages From Other Players.
    - /sun Sets The Weather To Sun
    - /rain Sets The Weather To Rain/Snow
    - /kickall Kicks Every Player Online, Except The Player Executing The Command
    - /banall Is A More Severe Version Of /kickall, Banning Every Player (Except, Of Course, The Player Executing The Command)
    - /suicide Kills The Player, But Gives Them The Option To Go Back To Their Death Point. If commands.deleteinventory Is Set To True, Their Inventory Is Cleared, With No Way To Get It Back.
    - /protect Sets The World Edit Selection To Not Be Editable Without The Permission xplay.editprotect
    - /feed Sets The Player's Hunger Points To Maximum.
    - /heal Sets All THe Player's Hearts And Hunger Points To Maximum.
    - /gm Is A Alias For /gamemode, and Acts Exactly The Same As The Default Command, But Has Some Extra Options, Such As c Or 1 For Creative Mode.

--- src/main/java/com/github/xspigot/XPlay.java
+++ src/main/java/com/github/xspigot/XPlay.java
@@ -9,6 +9,7 @@ import com.github.xspigot.events.JoinEvent;
 import com.github.xspigot.events.QuitEvent;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
@@ -22,6 +23,28 @@ import java.util.logging.Logger;
 //CONTRIBUTORS
 //EPICGAMER73
 //WSMAN217
+
+//Int21R4 - Planned Patch Notes:
+//
+//- Added Custom Vault Economy
+//- Added Feature To Teleport The Player To The Lobby On Join
+//- Added Placeholder API Support For All Config Values
+//- Added API Support
+//- Added Grief Protection
+//- Added Grief Protection Regions
+//- Added 12 New Commands --> /forcejoin, /forceleave, /togglechat, /sun, /rain, /kickall, /banall, /suicide, /protect, /feed, /heal, and /gm
+//- /forcejoin Fakes A Player To Join, And Sends A Message To Everyone In The Server That The Player Has Joined
+//- /forceleave Does The Same Thing As /forcejoin, But Makes The Player Leave Instead. If The Targeted Player Is Online, Then It Will Kick Them With A Customizable Error Message.
+//- /togglechat Allows The Player To Toggle Receiving Messages From Other Players.
+//- /sun Sets The Weather To Sun
+//- /rain Sets The Weather To Rain/Snow
+//- /kickall Kicks Every Player Online, Except The Player Executing The Command
+//- /banall Is A More Severe Version Of /kickall, Banning Every Player (Except, Of Course, The Player Executing The Command)
+//- /suicide Kills The Player, But Gives Them The Option To Go Back To Their Death Point. If commands.deleteinventory Is Set To True, Their Inventory Is Cleared, With No Way To Get It Back.
+//- /protect Sets The World Edit Selection To Not Be Editable Without The Permission xplay.editprotect
+//- /feed Sets The Player's Hunger Points To Maximum.
+//- /heal Sets All THe Player's Hearts And Hunger Points To Maximum.
+//- /gm Is A Alias For /gamemode, and Acts Exactly The Same As The Default Command, But Has Some Extra Options, Such As c Or 1 For Creative M
 
 public final class XPlay extends JavaPlugin implements Listener {
 
@@ -57,6 +80,8 @@ public final class XPlay extends JavaPlugin implements Listener {
             getLogger().log(Level.WARNING, "Developer Releases Are Not Stable - Use At Your Own Risk");
         }
 
+        Metrics metrics = new Metrics(this, 87463);
+
         new UpdateCheck(this, 87463).getVersion(version -> {
             if (!(this.getDescription().getVersion().equalsIgnoreCase(version))) {
                 getLogger().log(Level.WARNING, "A New Update For XPlay Is Avaliable!");
@@ -67,7 +92,7 @@ public final class XPlay extends JavaPlugin implements Listener {
 
         if (!setupEconomy()) {
             getLogger().log(Level.SEVERE, "Vault Is Not Installed - Install Vault And Restart The Server");
-            getLogger().log(Level.INFO, "If This Error Persists, Check If Vault Is Correctly Installed");
+            getLogger().log(Level.INFO, "If This Error Persists, Check If  Vault Is Correctly Installed");
             getServer().getPluginManager().disablePlugin(this);
         }
 
@@ -81,7 +106,6 @@ public final class XPlay extends JavaPlugin implements Listener {
         getLogger().log(Level.INFO, "Disabling Plugin");
         getLogger().log(Level.INFO, "Running: " + getDescription().getVersion());
         getLogger().log(Level.INFO, "SpigotMC Release");
-        getLogger().log(Level.INFO, "Error? (Coming Soon) bit.ly/xplayerror");
     }
 
     private void setUpCommands() {
